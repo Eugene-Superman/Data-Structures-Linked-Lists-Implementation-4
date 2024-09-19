548fa36b651e07a9d31b42ad2c3b9804be69af59
Eugene <31414509+Eugene-Superman@users.noreply.github.com>
2024-09-19 15:51:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,114 @@
+// add a method remove() to the linked list that deletes a node to the specified index.
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+    return array;
+  }
+  insert(index, value) {
+    //Check for proper parameters;
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = {
+      value: value,
+      next: null
+    }
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    // Check Parameters
+    if (index > this.length - 1) {
+      return this.printList();
+    }
+
+    if (!index && this.length === 1) {
+      this.head = {};
+      this.tail = {};
+
+      this.length = 0;
+      return this.printList();
+    }
+
+    if (index === 0) {
+      this.head = this.head.next;
+    } else if (index === this.length - 1) {
+      const leader = this.traverseToIndex(index - 1)
+      leader.next = null;
+      this.tail = leader;
+    } else {
+      const leader = this.traverseToIndex(index - 1)
+      const itemToDelete = leader.next
+      leader.next = itemToDelete.next
+    }
+
+
+    this.length--;
+    return this.printList();
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16); myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+console.log(myLinkedList.printList());
+myLinkedList.remove(2);
+console.log(myLinkedList.printList());
+myLinkedList.remove(4);
+console.log(myLinkedList.printList());
+myLinkedList.remove(0);
+console.log(myLinkedList.printList());
+myLinkedList.remove(111111111);
+console.log(myLinkedList.printList());
+
